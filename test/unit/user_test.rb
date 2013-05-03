6909b445e989fa0531b9da062f8d855b944c3dc2
Sharath Kumar <sharathkumarmn@gmail.com>
2013-05-03 08:57:14 +0530

    Added flash

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,16 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "a user should enter a first name" do
+  	user = User.new
+  	assert !user.save
+  	assert	!user.errors[:first_name].empty?
+  end
+
+  test "a user should unique profile name" do
+  	user = User.new
+  	assert !user.save
+  	assert	!user.errors[:profile_name].empty?
+  end
 end
